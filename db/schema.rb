448da80c2534e48688f496b6d9eb899e64afdc8e
Gabriel <perezg2@rpi.edu>
2013-09-23 15:48:38 -0400

    Modeling Courses and Registrations

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130921173736) do
+ActiveRecord::Schema.define(version: 20130923194538) do
+
+  create_table "courses", force: true do |t|
+    t.string   "name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "registrations", force: true do |t|
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "users", force: true do |t|
     t.string   "username"
