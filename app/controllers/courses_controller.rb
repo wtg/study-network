f07f7d29a77fcc0d34b5d9390b42403e9e7f631c
Gabriel <perezg2@rpi.edu>
2013-11-16 16:45:28 -0500

    Importing courses

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -19,14 +19,32 @@ class CoursesController < ApplicationController
 
     @classes = Array.new
     dept_classes = Array.new
+    a_class = Array.new
 
     page.search('table').each do |table|
-      table.search('td').each do |td|
-        dept_classes << td.content
+      table.search('tr').each do |tr|
+        if tr.search('td')[0] and tr.search('td')[1]
+          if tr.search('td')[1].text.strip != 'NOTE:'
+            a_class << tr.search('td')[0].text.strip
+            a_class << tr.search('td')[1].text.strip
+          end
+        end
+        dept_classes << a_class.dup
+        a_class.clear
       end
       @classes << dept_classes.dup
       dept_classes.clear
     end
+
+    @classes.each do |dept|
+      dept.each do |cl|
+        @course = Course.new(crn_course_sec: cl[0], title: cl[1])
+        @course.save
+      end
+    end
+
+    redirect_to 'courses_path'
+
   end
 
 end
